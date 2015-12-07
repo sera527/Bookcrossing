a9c2826410d4d46a7ac78fef9810042b33390364
Ярослав Репета < >
2015-12-07 02:14:23 +0200

    Select books from db is working!

---
+++ app/Books.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bookcrossing\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Books extends Model
+{
+    //
+}
---
+++ app/Http/Controllers/Index.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Bookcrossing\Http\Controllers;
+
+use Bookcrossing\Models\Books;
+use Illuminate\Http\Request;
+
+use Bookcrossing\Http\Requests;
+use Bookcrossing\Http\Controllers\Controller;
+
+class Index extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Books::all();
+        return view('index', ['books' => $books]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
