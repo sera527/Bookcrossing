e3e0ae60a4e0cdcb19f211b2794a7ac23e82ffe4
Сергій <serpos95@gmail.com>
2015-12-15 22:18:19 +0200

    delete GoogleAnalytics

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -36,16 +36,6 @@
             '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-NF6RMN');</script>
 <!-- End Google Tag Manager -->
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-65856084-2', 'auto');
-    ga('send', 'pageview');
-
-</script>
 <div id="vk_api_transport"></div>
 <script type="text/javascript">
     window.vkAsyncInit = function () {
